90e8fd13bc4defa73c22bdacd4b4d2039b7c9afd
franciscocavallaro <ccavallaro00@gmail.com>
2025-09-29 17:21:38 -0300

    chore: remove deprecated API entry point file as part of cleanup

--- api/index.mjs
+++
@@ -1,6 +0,0 @@
-// Single serverless function exporting the compiled Express app.
-// The app already mounts `app.use('/api', router)` internally.
-import app from "../dist/index.js";
-export default app;
-
-

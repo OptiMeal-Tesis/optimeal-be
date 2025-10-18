e7cfce2dfc2a9e8d5393507b008cf07209fef733
valentinavalenzi <92260481+valentinavalenzi@users.noreply.github.com>
2025-10-18 09:54:41 -0300

    fix: added last name and changed validation messages to spanish

--- src/domains/user/dto/user.dto.ts
+++ src/domains/user/dto/user.dto.ts
@@ -3,35 +3,37 @@ import { UserTypeEnum } from '@prisma/client';
 
 // Create user DTO validation schema
 export const CreateUserInputDTO = z.object({
-    email: z.string().email('Invalid email format'),
-    password: z.string().min(8, 'Password must be at least 8 characters'),
-    name: z.string().min(2, 'Name must be at least 2 characters').optional(),
-    national_id: z.string().regex(/^\d{7,10}$/, 'National ID must be 7-10 digits'),
+    email: z.string().email('Formato de email inválido'),
+    password: z.string().min(8, 'La contraseña debe tener al menos 8 caracteres'),
+    name: z.string().min(2, 'El nombre debe tener al menos 2 caracteres').optional(),
+    last_name: z.string().min(2, 'El apellido debe tener al menos 2 caracteres').optional(),
+    national_id: z.string().regex(/^\d{7,10}$/, 'El DNI debe tener entre 7 y 10 dígitos'),
     role: z.nativeEnum(UserTypeEnum).optional().default(UserTypeEnum.USER),
     cognito_sub: z.string().optional(),
 });
 
 // Update user DTO validation schema
 export const UpdateUserInputDTO = z.object({
-    name: z.string().min(2, 'Name must be at least 2 characters').optional(),
-    national_id: z.string().regex(/^\d{7,10}$/, 'National ID must be 7-10 digits').optional(),
+    name: z.string().min(2, 'El nombre debe tener al menos 2 caracteres').optional(),
+    last_name: z.string().min(2, 'El apellido debe tener al menos 2 caracteres').optional(),
+    // national_id: z.string().regex(/^\d{7,10}$/, 'El DNI debe tener entre 7 y 10 dígitos').optional(),
     role: z.nativeEnum(UserTypeEnum).optional(),
 });
 
 // Get user by ID DTO validation schema
 export const GetUserByIdInputDTO = z.object({
-    // id: z.number().int().positive('User ID must be a positive integer'),
-    id: z.string().regex(/^\d+$/, 'ID must be a number').transform(Number)
+    // id: z.number().int().positive('El ID del usuario debe ser un entero positivo'),
+    id: z.string().regex(/^\d+$/, 'El ID debe ser un número').transform(Number)
 });
 
 // Get user by email DTO validation schema
 export const GetUserByEmailInputDTO = z.object({
-    email: z.string().email('Invalid email format'),
+    email: z.string().email('Formato de email inválido'),
 });
 
 // Get user by national ID DTO validation schema
 export const GetUserByNationalIdInputDTO = z.object({
-    national_id: z.string().regex(/^\d{7,10}$/, 'National ID must be 7-10 digits'),
+    national_id: z.string().regex(/^\d{7,10}$/, 'El DNI debe tener entre 7 y 10 dígitos'),
 });
 
 // Type exports
